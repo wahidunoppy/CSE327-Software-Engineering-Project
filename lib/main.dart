64b1b8e6d6527fe7e68a5b4c50e37ed4c98147ec
tonorashedul <tono.rashedul@gmail.com>
2023-05-31 03:40:31 +0600

    Firestore Connection develop

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot)
+      {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          {
+            return const MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('Tutors Planet application is being initialized',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                ),
+              )
+            );
+          }
+        else if(snapshot.hasError)
+        {
+          return  const MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('An error has been occurred',
+                    style: TextStyle(
+                        color: Colors.cyan,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              )
+          );
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Tutors Planet App',
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
+            primarySwatch: Colors.blue,
+          ),
+          home: const Scaffold(),
+        );
+      }
+    );
+  }
+}
