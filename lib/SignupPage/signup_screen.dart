f239f1d9b427f47585ac96550ad9ac23d2372957
tonorashedul <tono.rashedul@gmail.com>
2023-06-09 03:18:58 +0600

    Name filed updated

--- lib/SignupPage/signup_screen.dart
+++ lib/SignupPage/signup_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,11 @@ class _SignUpState extends State<SignUp>with TickerProviderStateMixin {
 
   late Animation<double> _animation;
   late AnimationController _animationController;
+  final TextEditingController _fullNameController = TextEditingController(text:  '');
+  FocusNode _emailFocusNode = FocusNode();
+
+  final _signUpFormkey = GlobalKey <FormState>();
+  File? imageFile;
 
   @override
   void dispose() {
@@ -39,6 +46,7 @@ class _SignUpState extends State<SignUp>with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    Size size =MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -53,6 +61,77 @@ class _SignUpState extends State<SignUp>with TickerProviderStateMixin {
             height: double.infinity,
             fit: BoxFit.cover,
             alignment: FractionalOffset(_animation.value, 0),
+          ),
+          Container(
+            color: Colors.black54,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 80 ),
+              child: ListView(
+                children: [
+                  Form(
+                    key:  _signUpFormkey,
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            // create show image
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: size.width *0.24,
+                              height: size.width * 0.24,
+                              decoration: BoxDecoration(
+                                border: Border.all(width: 1, color: Colors.cyanAccent,),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: imageFile ==null
+                                  ?const Icon(Icons.camera_enhance_sharp,color: Colors.cyan, size: 30,)
+                                    :Image.file(imageFile!, fit: BoxFit.fill,)
+
+                              ),
+                            ),
+                          ),
+                        ),
+                        TextFormField(
+                          textInputAction: TextInputAction.next,
+                          onEditingComplete: () => FocusScope.of(context).requestFocus(_emailFocusNode),
+                          keyboardType: TextInputType.name,
+                          controller: _fullNameController,
+                          validator: (value)
+                          {
+                            if(value!.isEmpty )
+                            {
+                              return 'This Field is missing';
+                            }
+                            else{
+                              return null;
+                            }
+                          },
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                              hintText: 'Name',
+                              hintStyle: TextStyle(color: Colors.white),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              errorBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              )
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ),
+
           ),
         ],
       ),
